63a040fa2ea7b2460927c504850220c68ca084a5
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-04-16 13:04:24 -0300

    Pruebas con for, if y funciones, de forma secuencial

--- main.go
+++ main.go
@@ -4,6 +4,34 @@ import "fmt"
 
 func main() {
 
-	fmt.Print("prueba")
+	fmt.Println("prueba")
+
+	fmt.Println("Prueba")
+
+	foo()
+
+	fmt.Println("Algo mas")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+	}
+
+	bar()
 
 }
+
+func foo() {
+	fmt.Println("Estoy en foo")
+}
+
+func bar() {
+	fmt.Println("Salimos")
+}
+
+//control flow:
+//1 Secuencial
+//2  bucle iterativo
+//condicional
