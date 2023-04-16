5a3fcdc03bd72744c2963def06a06c51297e7971
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-04-16 19:30:36 -0300

    Lo estas haciendo bien :D, inicialización de variables y declaracion corta de variables

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
-package main
+learpackage main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Short declaration operator
+// Declaro una variable y le asigno un valor
+// Utilizar := Para declarar variables dentro de funciones, tiene alcance desde la declaracion hasta el final de la función
+// Una variable sin una inicializacion es igual a 0
+// False para booleans, 0 para integers, 0.0 para floats, "" vacios para strings,
+// y nil para pointer, funciones, interfaces, slcies, channels y mapas.
+// La variable ab la declaramos fuera de una funcion sin necesidad de darle un valor (puede ser int, float, string)
+// Declarar e iniciar "var ab=2", por ejemplo, es incicializar la variable
+var ab int
+var az int
 
 func main() {
 
+	x := 4
+
+	b := "Amongus"
+
+	var a int = 23 //utilizzar la palabra var para poder declarar variables fuera de funciones, tiene alcance en todo el package
+
+	var z int
+
+	z = a + x
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(x)
+	fmt.Println(z)
 	fmt.Println("prueba")
 
 	fmt.Println("Prueba")
