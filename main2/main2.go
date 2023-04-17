f9e0dc9041d1e1dbdffc8f04d77aad46f8eb2da9
lauti2311 <lautaro_ismael2010@hotmail.com>
2023-04-16 21:28:07 -0300

    Vamo bien :D

--- main2/main2.go
+++ main2/main2.go
@@ -6,10 +6,23 @@ func swap(x, y string, c int) (string, string, int) {
 	return y, x, c
 }
 
+func sumamongus(valor int) (resultado1, resultado2 int) {
+	valor = 5
+
+	resultado1 = valor * 2
+
+	resultado2 = valor + 2
+
+	return
+}
+
 func main() {
 
-	c := 1
+	var c int
+	var d int
 	a, b, c := swap("hello", "world", 2)
 
 	fmt.Println(a, b, c)
+	fmt.Println(sumamongus(d))
+
 }
